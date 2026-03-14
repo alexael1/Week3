415593c39d198ef671401dba0a65ac23af55337c
alexael1 <alexphro@proton.me>
2026-03-14 13:00:11 +0000

    Def

---
+++ week4.py
@@ -0,0 +1,62 @@
+#print("Select a number:")
+#print("1 - Add two numbers")
+#print("2 - Substract two numbers")
+
+#choice = input("Enter 1 or 2: ")
+
+#if choice == "1":
+#    number1 = int(input("Enter First number: "))
+#    number2 = int(input("Enter second number: "))
+#    total = number1 + number2
+#    print("Total: ", total)
+    
+#elif choice == "2":
+#    number1 = int(input("Enter First number: "))
+#    number2 = int(input("Enter second number: "))
+#    total = number1 - number2
+#    print("Total: ", total)
+    
+#else:
+#    print("Invalid number! Select 1 or 2")
+
+
+#def calculator():
+#    num1 = 10
+#    num2 = 20
+#    total = num1 + num2
+#    print("Total is", total)
+       
+#calculator()
+
+
+
+#def calculator():
+#    num1 = 10
+#    num2 = 20
+#    total = num1 + num2
+#    return total
+       
+#sum = calculator()
+#print("Return value is : ", sum)
+
+        
+        
+#def add(num1, num2):
+#    total = num1 + num2
+#    return total
+
+#def average(total):
+#    avg = total/2
+#    return avg
+#math = int(input("Enter math marks: "))
+#bio = int(input("Enter bio makrs: "))
+#total = add(math,bio)
+#marks_average = average (total)
+
+#print("Total marks are: ", total, "And average is: ", marks_average)
+
+
+
+
+
+
